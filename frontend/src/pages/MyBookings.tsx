a9310f701e79ad28cb784bce4487341749e9271b
Worthy M <mandalbasant330@yahoo.com>
2024-06-24 20:49:19 +0530

    minor change

--- frontend/src/pages/MyBookings.tsx
+++ frontend/src/pages/MyBookings.tsx
@@ -57,22 +57,22 @@ const MyBookings = () => {
   return (
     <main className="space-y-5">
       {/* User Header */}
-      <div className="bg-white border-b py-4 px-10 d:py-7">
+      <div className="py-4 px-10">
         <div className="mx-auto max-w-6xl w-full">
-          <div className="text-goldColor font-medium text-3xl leading-30px tracking-extraTight d:text-35 d:leading-10 mb-2">
+          <div className="text-goldColor font-medium text-3xl leading-30px tracking-extraTight mb-2">
             {personalInfo.name}
           </div>
-          {/* <div className="leading-relaxed d:text-17 d:leading-8">
+          {/* <div className="leading-relaxed">
             $0 credits available
           </div> */}
         </div>
       </div>
       {/* User Header Ends */}
 
-      <div className="mx-auto max-w-6xl w-full d:flex d:flex-row d:items-start d:pt-5 pt-5">
+      <div className="mx-auto max-w-6xl w-full pt-5">
         <h1 className="text-2xl text-center lg:text-left font-medium mb-5 pl-0 lg:pl-5">My Bookings</h1>
         {/* Tabs */}
-        <div className="d:w-9/12">
+        <div className="">
           <section className="py-4">
             <ul className="overflow-x-auto flex flex-row px-5 gap-3">
               <li className="">
@@ -102,16 +102,16 @@ const MyBookings = () => {
             </ul>
             {/* Content Shown when clicked on Bookings*/}
             {activeTab === "bookings" && (
-              <div className="mx-5 my-4 pt-5 rounded-xl shadow-lg border">
-                <h3 className="border-b capitalize text-xl leading-[30px] font-medium pb-4 px-4 d:border-b-0 d:px-5 text-goldColor">
+              <div className="mx-5 my-4 pt-5">
+                <h3 className="border-b border-goldColor capitalize text-xl leading-[30px] font-medium pb-4 px-4 text-goldColor">
                   My Bookings
                 </h3>
-                <div className="">
+                <div className="space-y-5 rounded-xl shadow-lg">
                   {bookingData && bookingData.length > 0 ? (
                     bookingData.map((hotel:any) => (
                       <div
                         key={hotel._id}
-                        className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] border border-slate-300 rounded-lg p-8 gap-5"
+                        className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] border border-goldColor rounded-lg p-8 gap-5"
                       >
                         <div className="lg:w-full lg:h-[250px]">
                           <img
@@ -123,7 +123,7 @@ const MyBookings = () => {
                           <div className="text-2xl font-bold">
                             {hotel.name}
                             <div className="text-xs font-normal">
-                              {hotel.city}, {hotel.state}, In
+                              {hotel.city}, {hotel.state}, India
                             </div>
                           </div>
                           {hotel.bookings.map((booking:any) => (
@@ -158,21 +158,21 @@ const MyBookings = () => {
 
             {/* Content Shown when clicked on Favourites*/}
             {activeTab === "favourites" && (
-              <div className="mx-5 my-4 pt-5 rounded-xl shadow-lg border">
-                <h3 className="border-b capitalize text-xl leading-[30px] font-medium pb-4 px-4 d:border-b-0 d:px-5 text-goldColor">
+              <div className="mx-5 my-4 pt-5">
+                <h3 className="border-b capitalize text-xl leading-[30px] font-medium pb-4 px-4 text-goldColor">
                   My Favourites
                 </h3>
-                <div className="">
+                <div className="space-y-5 rounded-xl shadow-lg">
                 {favourites && favourites.length > 0 ? (
                     favourites.map((hotel:any) => (
                       <Link
                         to={`/detail/${hotel._id}`}
-                        className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] border border-slate-300 rounded-lg p-8 gap-5"
+                        className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] border border-goldColor rounded-lg p-8 gap-5"
                       >
                         <div className="lg:w-full lg:h-[250px]">
                           <img
                             src={hotel.imageUrls[0]}
-                            className="w-full h-full object-cover object-center"
+                            className="w-full h-full object-cover object-center rounded-md"
                           />
                         </div>
                         <div className="flex flex-col gap-4 overflow-y-auto max-h-[300px]">
@@ -197,7 +197,7 @@ const MyBookings = () => {
             {/* Content Shown when clicked on Waitlist*/}
             {activeTab === "waitlist" && (
               <div className="mx-5 my-4 pt-5 rounded-xl shadow-lg border">
-                <h3 className="border-b capitalize text-xl leading-[30px] font-medium pb-4 px-4 d:border-b-0 d:px-5 text-goldColor">
+                <h3 className="border-b capitalize text-xl leading-[30px] font-medium pb-4 px-4 text-goldColor">
                   My Waitlist
                 </h3>
                 <div className="">
