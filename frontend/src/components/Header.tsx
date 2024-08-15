56e1d0ec8434901292c2c3524cc34260dde27d9b
Worthy M <mandalbasant330@yahoo.com>
2024-08-15 12:05:28 +0530

    Pages removed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -829,14 +829,14 @@ const Header = () => {
                     id="user-dropdown"
                   >
                     <ul className="py-2" aria-labelledby="user-menu-button">
-                      <li onClick={toggleDropdown}>
+                      {/* <li onClick={toggleDropdown}>
                         <Link
                           to="#"
                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
                           Profile
                         </Link>
-                      </li>
+                      </li> */}
                       <li onClick={toggleDropdown}>
                         <Link
                           to="/my-bookings"
