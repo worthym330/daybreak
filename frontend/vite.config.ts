f3cf0d09de03f476220516c3f76e2ae84d310d54
Worthy M <mandalbasant330@yahoo.com>
2024-10-25 09:59:26 +0530

    Config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
   optimizeDeps: {
-    include: ['redux-thunk'],
+    include: ['redux-thunk', 'react', 'react-dom'],
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+    cssCodeSplit: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
   },
 });
